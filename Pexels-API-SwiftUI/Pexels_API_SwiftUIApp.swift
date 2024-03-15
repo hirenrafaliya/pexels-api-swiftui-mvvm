bab857fbf3d20de4a8b21e61cbe26d4f343a8aae
Hiren Rafaliya <hiren6997@gmail.com>
2024-03-15 18:46:11 +0530

    Initial Commit

---
+++ Pexels-API-SwiftUI/Pexels_API_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pexels_API_SwiftUIApp.swift
+//  Pexels-API-SwiftUI
+//
+//  Created by Hiren Rafaliya on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Pexels_API_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
